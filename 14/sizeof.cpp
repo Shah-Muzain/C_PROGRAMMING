6ce8513537693577abb31f6301cb46d83e66d96b
Shah-Muzain <166078711+Shah-Muzain@users.noreply.github.com>
2024-05-21 14:04:11 +0530

    Add files via upload

---
+++ 14/sizeof.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    //printf("Size of printf function: %lu bytes\n", sizeof(printf));
+    printf("Size of int data type: %d bytes\n", sizeof(int));
+    printf("Size of char data type: %d bytes\n", sizeof(char));
+    printf("Size of float data type: %d bytes\n", sizeof(float));
+    printf("Size of double data type: %ld bytes\n", sizeof(double));
+    printf("Size of long double data type: %d bytes\n", sizeof(long double));
+
+    return 0;
+}
